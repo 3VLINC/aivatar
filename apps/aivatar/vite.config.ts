f03f83cb773e5b000fe6a583dce88dbc1fb1d84e
Dan Imbrogno <dan@3vl.ca>
2025-02-07 22:32:35 -0500

    fallback https

--- apps/aivatar/vite.config.ts
+++ apps/aivatar/vite.config.ts
@@ -5,14 +5,22 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const getHttps = () => {
+  try {
+
+    return {
+      key: readFileSync(resolve(__dirname, './dev/origin.key')),
+      cert: readFileSync(resolve(__dirname, './dev/origin.cert')),
+    }
+  } catch (e) {
+    return undefined;
+  }
+}
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   server: {
     allowedHosts: ["dev.3vl.ca", "aivatar.3vl.ca", "localhost"],
-    https: {
-      key: readFileSync(resolve(__dirname, './dev/origin.key')),
-      cert: readFileSync(resolve(__dirname, './dev/origin.cert')),
-    },
+    https: getHttps(),
     port: 443,
     proxy: {
       // ...
